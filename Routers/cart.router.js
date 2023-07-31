4ca1274770a2e7ae10cfe3b761f46b6c40f2d75c
Hilcbek <balemayehu07@gmail.com>
2023-07-31 20:38:46 +0300

    added backend 3

--- Routers/cart.router.js
+++ Routers/cart.router.js
@@ -1,9 +1,9 @@
 import express from 'express'
 import { AddToCart, AllCart, RemoveAllCartFromUser, RemoveItemFromCart, cartToUser } from '../controllers/cart.controller.js';
-import { verifyIsUserLoggedIn } from '../jwt/token.js';
+import { verifyIsUserLoggedIn, verifyisUserAdmin } from '../jwt/token.js';
 export let routerCart = express.Router();
 routerCart.post('/addtocart/:id',verifyIsUserLoggedIn ,AddToCart)
 routerCart.get('/loggedusercart', verifyIsUserLoggedIn, cartToUser)
 routerCart.delete('/removecart/:id',verifyIsUserLoggedIn ,RemoveItemFromCart)
-routerCart.get('/',verifyIsUserLoggedIn, AllCart)
+routerCart.get('/',verifyisUserAdmin,verifyIsUserLoggedIn, AllCart)
 routerCart.delete('/', verifyIsUserLoggedIn, RemoveAllCartFromUser);
