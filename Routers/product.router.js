5119e8808c048ed4c39cbe59d5278f8e58c272bb
Bereket Alemayehu <86413773+Hilcbek@users.noreply.github.com>
2023-11-10 21:26:59 +0300

    Update product.router.js

--- Routers/product.router.js
+++ Routers/product.router.js
@@ -2,5 +2,5 @@ import express from 'express'
 import { Add, AllProducts } from '../controllers/products.controller.js';
 import { verifyIsUserLoggedIn } from '../jwt/token.js';
 export let routerPorduct = express.Router();
-routerPorduct.post('/add',verifyIsUserLoggedIn, Add)
-routerPorduct.get('/',verifyIsUserLoggedIn, AllProducts)
+routerPorduct.post('/add', Add)
+routerPorduct.get('/', AllProducts)
