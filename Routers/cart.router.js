6876b1ba3fc5dc306abdbb9c7bc75d8664162be2
Bereket Alemayehu <86413773+Hilcbek@users.noreply.github.com>
2023-11-10 21:28:01 +0300

    Update cart.router.js

--- Routers/cart.router.js
+++ Routers/cart.router.js
@@ -2,8 +2,8 @@ import express from 'express'
 import { AddToCart, AllCart, RemoveAllCartFromUser, RemoveItemFromCart, cartToUser } from '../controllers/cart.controller.js';
 import { verifyIsUserLoggedIn, verifyisUserAdmin } from '../jwt/token.js';
 export let routerCart = express.Router();
-routerCart.post('/addtocart/:id',verifyIsUserLoggedIn ,AddToCart)
-routerCart.get('/loggedusercart', verifyIsUserLoggedIn, cartToUser)
-routerCart.delete('/removecart/:id',verifyIsUserLoggedIn ,RemoveItemFromCart)
-routerCart.get('/',verifyisUserAdmin, AllCart)
-routerCart.delete('/', verifyIsUserLoggedIn, RemoveAllCartFromUser);
+routerCart.post('/addtocart/:id' ,AddToCart)
+routerCart.get('/loggedusercart', cartToUser)
+routerCart.delete('/removecart/:id' ,RemoveItemFromCart)
+routerCart.get('/', AllCart)
+routerCart.delete('/', RemoveAllCartFromUser);
